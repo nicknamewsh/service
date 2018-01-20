a877d99b4ab7763b0fe68fd6ec477f7177ac9bbd
nicknamewsh <nicknamewsh@users.noreply.github.com>
2018-01-20 16:41:41 +0800

    添加log日志

--- springcloud-demo/src/main/java/com/firstspringcloud/springclouddemo/controller/TestController.java
+++ springcloud-demo/src/main/java/com/firstspringcloud/springclouddemo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.firstspringcloud.springclouddemo.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/spring_cloud")
 public class TestController {
+    private Logger logger = Logger.getLogger(TestController.class);
     @Value("${test.tt}")
     private String msg;
     @Value("${server.port}")
     private String port;
+
     @RequestMapping("/test")
     public String test(){
         return msg;
@@ -23,6 +26,7 @@ public class TestController {
     //获取随机端口号
     @RequestMapping("/get_random_port")
     public String get_random_port(){
+        logger.info("========================================获取随机端口号======================================");
         return this.port;
     }
 }
