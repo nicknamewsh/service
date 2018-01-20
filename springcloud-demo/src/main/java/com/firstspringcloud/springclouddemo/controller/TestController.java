4ef386852ff234e4c03d3ffc002372bbe6e1725a
nicknamewsh <nicknamewsh@users.noreply.github.com>
2018-01-20 16:02:26 +0800

    配置变量的引用

--- springcloud-demo/src/main/java/com/firstspringcloud/springclouddemo/controller/TestController.java
+++ springcloud-demo/src/main/java/com/firstspringcloud/springclouddemo/controller/TestController.java
@@ -1,14 +1,17 @@
 package com.firstspringcloud.springclouddemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/spring_cloud")
 public class TestController {
+    @Value("${test.tt}")
+    private String msg;
     @RequestMapping("/test")
     public String test(){
-        return "Hello word";
+        return msg;
     }
 
     @RequestMapping("/tt")
